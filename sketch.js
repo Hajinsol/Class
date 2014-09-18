46e537a7a848942eed085941bbbea6b279964fdf
Hajinsol <jinsor5542@gmail.com>
2014-09-18 15:50:45 +0900

    1

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(640, 480);
+  background(01, 122, 183);
+  
+  noFill();
+
+    stroke(24, 234, 102);
+    
+}
+/*
+    var x;
+
+    x = 1;
+    ellipse(x * 150, 100, 50, 50);
+
+    x = 2;
+    ellipse(x * 150, 100, 50, 50);
+
+    x = 3;
+    ellipse(x * 150, 100, 50, 50);
+*/
+function draw(){
+//for (시작상태; 반복하는 조건; 증가값)
+
+  background(128, 0, 64);
+
+  ellipse(320, 240, mouseX, mouseY);
+
+ 
+}
